ede3d16b01ab8482545941ed755337e97d3f3730
AlexDeLarge <jmrendon48@gmail.com>
2021-04-19 02:55:50 -0700

    added user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,33 @@
+const route = require("express").Router();
+
+const router = require("../../../pizza-hunt/routes/api/pizza-routes");
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+// /api/users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend)
+
+module.exports = router;
